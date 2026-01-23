ac636e88f96c03fbfc858bcc052f1cead3d8c007
hmsarfrazhassan <hmsarfraz.hassan@gmail.com>
2026-01-23 12:56:00 +0500

    Added css on form and other elements

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [showForm, setShowForm] = useState(false);
-  const [users, setUser] = useState([]);
+  const [showForm, setShowForm] = useState(true);
   const [formFields, setFormFields] = useState({
     name: "",
     email: "",
-    password: "",
   });
+  const [users, setUsers] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUsers([...users, formFields]);
+    setFormFields({
+      name: "",
+      email: "",
+    });
+    setShowForm(false);
+  };
 
   const handleChange = (e) => {
     setFormFields({
@@ -17,61 +26,51 @@ function App() {
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("form fields", formFields);
-    setUser([...users, formFields]);
-    setFormFields({
-      name: "",
-      email: "",
-      password: "",
-    });
-    setShowForm(false);
-  };
   return (
-    <>
-      <button onClick={() => setShowForm(true)}>Show Form</button>
+    <div className="container">
+      <button className="button" onClick={() => setShowForm(!showForm)}>
+        Toggle form
+      </button>
+
       {showForm && (
-        <form onSubmit={handleSubmit}>
-          <label>Name</label>
+        <form className="card" onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
           <input
             type="text"
             name="name"
             value={formFields.name}
             onChange={handleChange}
           />
-          <label>Email</label>
+          <label htmlFor="email">Email</label>
           <input
             type="email"
             name="email"
             value={formFields.email}
             onChange={handleChange}
           />
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            value={formFields.password}
-            onChange={handleChange}
-          />
-          <button type="submit">Submit</button>
+          <button className="submit" type="submit">
+            submit
+          </button>
         </form>
       )}
-      <table>
-        <tr>
-          <th>name</th>
-          <th>email</th>
-          <th>password</th>
-        </tr>
-        {users.map((user, index) => (
-          <tr key={index}>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
-            <td>{user.password}</td>
+
+      <table width={100} className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
-    </>
+    </div>
   );
 }
 
